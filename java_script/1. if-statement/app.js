f76403a26cb4722090a40b85f721b066fb6cd0b8
Mayong Saputra <gilang46622@gmail.com>
2025-04-03 17:42:38 +0700

    Built<Folder>:'Folder Excercise JavarScript'

---
+++ java_script/1. if-statement/app.js
@@ -0,0 +1,24 @@
+// if (1 === ){
+//     console.log('Benar Nilai itu sebanding!!');
+// }else{
+//     console.log("Salah Nilai itu tidak sebanding");
+// }
+
+// let angka = Math.random()
+
+// console.log(angka)
+// if (angka >= 0.5){
+//     console.log("Nilanya angka Lebih Besar dari sama dengan 0.5");
+// }else{
+
+//     console.log("Nilanya angka Lebih kecil dari sama dengan 0.5");
+// }
+
+
+nilai = prompt("Masukan Anka berapa aja :  ");
+
+if ( nilai<= 5){
+    console.log("Berhasil........");
+}else{
+    console.log("Sayang Sekali, Angka-nya besar");
+}
